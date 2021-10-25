7850ef88f5f87fcbe834fcf6e4c710e3e80eb4a2
ApuTHC <apux012@gmail.com>
2021-10-25 10:37:00 -0500

    index full

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
-    $(".btn-services").click(function (e) { 
+    setTimeout(() => {
+        footerTop();
+    }, 50);
+    function footerTop() {
+        var aux =$(".contenido-pagina").height()+$(".contenido-pagina").offset().top ;
+        $('footer').css('top',aux);
+    }
+    
+    $(window).resize(function () { 
+        setTimeout(() => {
+            footerTop();
+        }, 50);
+    });
+    $(window).scroll(function () { 
+        setTimeout(() => {
+            footerTop();
+        }, 50);
+    });
+    $("#pestania-accesibilidad").click(function (e) { 
         e.preventDefault();
-        $('.btn-services').toggleClass('active');
-        $('#services').toggleClass('active');
-        $('body').toggleClass('active');
+        setTimeout(() => {
+            footerTop();
+        }, 50);
     });
 });
